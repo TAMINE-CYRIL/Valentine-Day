a9addcd64278dab121e5de3ea3679feed2af7460
Xanooo <tamineha@yahoo.fr>
2025-02-10 02:24:12 +0100

    Ajout de la JSDoc, simplification d'une fonction.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let count = 0;
 let audio = new Audio("media/chill.mp3");
 let yippie = new Audio("media/yippie.mp3");
 
+/**
+ * Fonction créant les coeurs et les implémente dans le corps du site
+ */
 function createHeart() {
     let heart = document.createElement("section");
     heart.classList.add("heart");
@@ -15,10 +18,16 @@ function createHeart() {
     }, 3000);
 }
 
+/**
+ * Impose un délai avant l'apparition des coeurs
+ */
 function startHearts() {
     setInterval(createHeart, 300);
 }
 
+/**
+ * Augmente la taille du bouton
+ */
 function increaseButtonSize() {
     let buttonYes = document.querySelector(".yes"); // Sélectionne uniquement le bouton "Yes"
 
@@ -37,19 +46,31 @@ function increaseButtonSize() {
     count++;
     console.log(count);
     if (count === 8) {
-        buttonYes.style.fontSize = "5rem";
-        buttonYes.style.position = "fixed";
-        buttonYes.style.top = "0";
-        buttonYes.style.left = "0";
-        buttonYes.style.width = "calc(100vw - 20px)";
-        buttonYes.style.height = "calc(100vh - 20px)";
-        buttonYes.style.margin = "10px";
-
-        let buttonNo = document.querySelector(".no");
-        buttonNo.style.display = "none";
+        finalChoice(buttonYes)
     }
 }
 
+/**
+ * Résultat final en cas de 8 refus de cliquer sur le bouton Yes
+ * @param buttonYes
+ */
+function finalChoice(buttonYes){
+    buttonYes.style.fontSize = "5rem";
+    buttonYes.style.position = "fixed";
+    buttonYes.style.top = "0";
+    buttonYes.style.left = "0";
+    buttonYes.style.width = "calc(100vw - 20px)";
+    buttonYes.style.height = "calc(100vh - 20px)";
+    buttonYes.style.margin = "10px";
+
+    let buttonNo = document.querySelector(".no");
+    buttonNo.style.display = "none";
+}
+
+
+/**
+ * Crée et place le gif dans le corps du document
+ */
 function createGif(){
     let gifContainer = document.createElement("section");
     gifContainer.classList.add("gif-container");
@@ -65,12 +86,18 @@ function createGif(){
 
 }
 
+/**
+ * Gère les intérations lorsque le bouton No est appuyé.
+ */
 function no() {
     console.log("C'est cliqué !");
     let buttonNo = document.querySelector(".no");
     buttonNo.addEventListener("click", increaseButtonSize);
 }
 
+/**
+ * Gère les intéractions lorsque le bouton Yes est appuyé
+ */
 function yes() {
     let buttonNo = document.querySelector(".no");
     let buttonYes = document.querySelector(".yes");
@@ -86,6 +113,9 @@ function yes() {
     startHearts();
 }
 
+/**
+ * Change le site si le bouton caché est trouvé
+ */
 function gay() {
     let buttonNo = document.querySelector(".no");
     let buttonYes = document.querySelector(".yes");
@@ -96,6 +126,7 @@ function gay() {
     buttonYes.textContent = "Yes I am.";
     alert("You have good eyes ! 👀");
 }
+
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttonNo = document.querySelector(".no");
